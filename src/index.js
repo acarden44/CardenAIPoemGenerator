239fa41413228be5fd4585553cb12744e35c4178
april-lcv <106687951+april-lcv@users.noreply.github.com>
2025-02-07 12:09:55 -0500

    incorporated js and api

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
-function generatePoem(event) {
-  event.preventDefault();
-
+function displayPoem(response) {
   new Typewriter("#poem", {
-    strings: "This is a poem",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+
+function generatePoem(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "3b34c40446ftcf4f07e329o00aa2e010";
+  let context =
+    "You are a marvel comic expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br />. Make sure to follow the user instructions but include a Marvel Comic flare. Do not include a title to the poem. Sign the poem with 'Carden AI' inside a <strong> element at the end of the poem and NOT at the beginning. Do not include html in the poem";
+  let prompt = `Generate a poem about  ${instructionsInput.value}`;
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a poem about ${instructionsInput.value}</div>`;
+
+  axios.get(apiURL).then(displayPoem);
 }
 
 let poemFormElement = document.querySelector("#poem-generator-form");
